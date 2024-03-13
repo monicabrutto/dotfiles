37c143ab5dda0c57349be57680b83f104ab225a6
Ubuntu <brutto.2@wright.edu>
2024-03-13 16:07:25 +0000

    added apt install to script

--- install.sh
+++ install.sh
@@ -7,9 +7,27 @@ if [[ "$(id -u)" -eq 0 ]]; then
 
 	echo $(which apt) 
 	if [ "$(which apt)" = "/usr/bin/apt" ]; then
-    	   echo "apt is installed exactly as specified."
-	else
-    	   echo "apt is not installed at the specified location."
+    		echo "apt is installed exactly as specified."
+		
+		apt install -y \
+			nmap \
+			dnsutils \
+			libgl1-mesa-glx \
+			libegl1-mesa \
+			libxrandr2 \
+		       	libxrandr2 \
+		       	libxss1 \
+			libxcursor1 \
+		       	libxcomposite1 \
+			libasound2 \
+			libxi6 \
+			libxtst6 
+		curl -O https://repo.anaconda.com/archive/Anaconda3-2024.02-1-Linux-x86_64.sh
+		bash :Anaconda3-2024.02-1-Linux-x86_64.sh
+	
+
+   	else
+    		echo "apt is not installed at the specified location."
 	fi
 
 
