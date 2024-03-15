bcae5edf5acd6b8e20d246846be165210a16f118
monicabrutto <111888056+monicabrutto@users.noreply.github.com>
2024-03-15 10:03:46 -0400

    Update install.sh
    Updated code based on given dotfiles

--- install.sh
+++ install.sh
@@ -1,39 +1,45 @@
 #!/bin/bash
+# get username that called script
+echo $SUDO_USER
+ME=$SUDO_USER
 
+# check if effective user id is 0 (root)
 
-#Check to see if the script is running as root or with sudo 
-if [[ "$(id -u)" -eq 0 ]]; then 
-	echo "script is running as root" 
-
-	echo $(which apt) 
-	if [ "$(which apt)" = "/usr/bin/apt" ]; then
-    		echo "apt is installed exactly as specified."
-		
+if [[ "$(id -u)" -eq 0 ]]; then
+	echo "Script is running as root"
+	# check if apt is package manager
+	# if apt is package manager and you run which apt it will specify a path to where its stored
+	echo $(which apt)
+	if [[ -n "$(which apt)" ]]; then
+		echo "apt is installed exactly as specified."
 		apt install -y \
 			nmap \
 			dnsutils \
+   			greed \ 
 			libgl1-mesa-glx \
 			libegl1-mesa \
 			libxrandr2 \
-		       	libxrandr2 \
-		       	libxss1 \
+			libxrandr2 \
+			libxss1 \
 			libxcursor1 \
-		       	libxcomposite1 \
+			libxcomposite1 \
 			libasound2 \
 			libxi6 \
-			libxtst6 
-		curl -O https://repo.anaconda.com/archive/Anaconda3-2024.02-1-Linux-x86_64.sh
-		bash :Anaconda3-2024.02-1-Linux-x86_64.sh
-	
-
-   	else
-    		echo "apt is not installed at the specified location."
+			libxtst6
+		if [[ -e $(ls Anaconda3*.sh 2> /dev/null | head -1) ]]; then
+			echo "Installer found, running it"
+		        bash Anaconda3-2024.02-1-Linux-x86_64.sh -b -p /home/$ME/anaconda3
+			echo "PATH=$PATH:/home/kduncan/anaconda3/bin" >> /home/$ME/.profile
+	        else
+	 		echo "Downloading anaconda installer"		
+			curl -O https://repo.anaconda.com/archive/Anaconda3-2024.02-1-Linux-x86_64.sh
+			bash Anaconda3-2024.02-1-Linux-x86_64.sh -b -p /home/$ME/anaconda3
+			echo "PATH=$PATH:/home/kduncan/anaconda3/bin" >> /home/$ME/.profile
+		fi
+	else
+		echo "apt is not installed at the specified location."
 	fi
-
-
-
-else 
-	echo "You must run with root permissions" 1>&2 
+else
+	echo "Script is not running as root, exiting..." 1>&2
 	exit 1
-
-fi 
+fi
