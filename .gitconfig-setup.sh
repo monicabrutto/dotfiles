a4a26fa5183ff5e4fb9ab9fa04eab846d068e0b4
monicabrutto <111888056+monicabrutto@users.noreply.github.com>
2024-03-15 20:50:40 -0400

    Update .gitconfig-setup.sh
    it does not work, circle back to later

--- .gitconfig-setup.sh
+++ .gitconfig-setup.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
-if [ -f ~/.gitconfig ]; then
-    echo "Existing .gitconfig file found. Do you want to overwrite it? (y/n)"
-    read -r response
-    if [[ $response =~ ^[Yy]$ ]]; then
-        rm ~/.gitconfig
+# if [ -f ~/.gitconfig ]; then
+#     echo "Existing .gitconfig file found. Do you want to overwrite it? (y/n)"
+#     read -r response
+#     if [[ $response =~ ^[Yy]$ ]]; then
+#         rm ~/.gitconfig
 
-	echo "Setting up .gitconfig file"
-	
-	# Set up .gitconfig file
-	cat > ~/.gitconfig <<EOF
-	[user]
-    		name = "Monica Brutto"
-    		email = "brutto.2@wright.edu"
-	[color]
-    		ui = auto
-	[core]
-    		editor = vim
-	[help]
-    		autocorrect = 20
-EOF
+# echo "Setting up .gitconfig file"
 
-	echo "Git configuration is set up!"
-   else
-        echo "Aborted. .gitconfig file was not modified."
-        exit 1
-    fi
-fi
+# # Set up .gitconfig file
+# cat > ~/.gitconfig <<EOF
+[user]
+	name = "Monica Brutto"
+	email = "brutto.2@wright.edu"
+[color]
+	ui = auto
+[core]
+	editor = vim
+[help]
+	autocorrect = 20
+# EOF
+
+# 	echo "Git configuration is set up!"
+#    else
+#         echo "Aborted. .gitconfig file was not modified."
+#         exit 1
+#     fi
+# fi
 
 
