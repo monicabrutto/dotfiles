2d587d810b20d88a36e754a1f7fc881da0ab034d
monicabrutto <111888056+monicabrutto@users.noreply.github.com>
2024-03-15 20:11:56 -0400

    Update install-aws-cli.sh
    hopefully this works lol

--- install-aws-cli.sh
+++ install-aws-cli.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
-
+#Taken from classmates, chat gpt and parts of the internet I have since lost
 install-aws-cli(){
 
+read -rp "[PROMPT] Do you want to install aws cli? (y/n):" choice 
 
-if [[ -n $(which unzip) ]]; then 
+choice=$(echo -e "$choice" | tr '[:upper:]' '[:lower:]')
 
-	sudo apt -y /
-	unzip
-	
+if [[ "$choice" == "y" | "$choice" == "yes" ]]; then 
+
+	if [[ -n $(which unzip) ]]; then 
+		sudo apt -y /
+		unzip
+	fi 
+ 
 	curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 	unzip awscliv2.zip
-	sudo ./aws/install
-
-
-
-
-
+	sudo ./aws/instal
+ elif [[ "$choice" == "n" || "$choice" == "no" ]]; then 
+ 	echo "not installing" 
+  	exit 0
+else 
+	echo "Invalid chocie, please enter either yes or no"
+ 	exit 1
+fi
 
 }
 
