835eb109d546882dce596d173127c7ebeec63f64
monicabrutto <111888056+monicabrutto@users.noreply.github.com>
2024-03-18 11:56:09 -0400

    Update install-aws-cli.sh
    Updated aws to actually install

--- install-aws-cli.sh
+++ install-aws-cli.sh
@@ -1,28 +1,22 @@
 #!/bin/bash
 #Taken from classmates, chat gpt and parts of the internet I have since lost
 install-aws-cli(){
-
-read -rp "[PROMPT] Do you want to install aws cli? (y/n):" choice 
-
-choice=$(echo -e "$choice" | tr '[:upper:]' '[:lower:]')
-
-if [[ "$choice" == "y" || "$choice" == "yes" ]]; then 
-
+echo "installing aws cli"
 	if [[ -n $(which unzip) ]]; then 
 		sudo apt -y /
 		unzip
 	fi 
  
-	curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-	unzip awscliv2.zip
-	sudo ./aws/instal
- elif [[ "$choice" == "n" || "$choice" == "no" ]]; then 
- 	echo "not installing" 
-  	exit 0
-else 
-	echo "Invalid chocie, please enter either yes or no"
- 	exit 1
-fi
+	if [[ -e $(ls awsdl/awscliv2.zip 2> /dev/null | head -1) ]]; then
+		echo "AWS CLI V2 installer found, running it"
+		unzip awsdl/awscliv2.zip -d awsdl/
+		./awsdl/aws/install 
+	else
+		mkdir awsdl
+		curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awsdl/awscliv2.zip"
+		unzip awsdl/awscliv2.zip -d awsdl/
+		./awsdl/aws/install
+	fi 
 
 }
 
